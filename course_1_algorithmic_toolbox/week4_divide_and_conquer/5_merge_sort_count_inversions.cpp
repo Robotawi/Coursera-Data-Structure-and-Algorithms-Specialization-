27ead16fba128e92ded75a216f283540be62ec36
Robotawi <mohamed.s.raessa@gmail.com>
2022-09-21 06:18:30 +0900

    Count inversions with merge sort

---
+++ course_1_algorithmic_toolbox/week4_divide_and_conquer/5_merge_sort_count_inversions.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_vector(vector<int> &sequence)
+{
+    for (auto &elem : sequence)
+    {
+        cout << elem << " ";
+    }
+    cout << "\n";
+}
+
+int merge(vector<int> &sequence, int l, int m, int r)
+{
+    int i = l;     // left array first idx
+    int j = m + 1; // right array first idx
+    int count = 0;
+
+    vector<int> temp(r - l + 1);
+
+    int k = 0; // temp array idx
+
+    while (i <= m && j <= r)
+    {
+        if (sequence.at(i) <= sequence.at(j))
+        {
+            temp.at(k) = sequence.at(i);
+            i++;
+            k++;
+        }
+        else
+        {
+            count = count + (m + 1 - i);//the tricky point. This is because all elements up to m+1 will
+            temp.at(k) = sequence.at(j);
+            j++;
+            k++;
+        }
+    }
+    while (i <= m)
+    {
+        temp.at(k) = sequence.at(i);
+        i++;
+        k++;
+    }
+    while (j <= r)
+    {
+        temp.at(k) = sequence.at(j);
+        j++;
+        k++;
+    }
+
+    for (int i = l; i <= r; ++i) // notice this start and end points. It can also be temp.size()-1
+    {
+        // the first element in temp goes to lth position in sequence
+        sequence.at(i) = temp.at(i - l);
+    }
+
+    return count;
+}
+
+int merge_sort(vector<int> &sequence, int l, int r)
+{
+    int count = 0;
+
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        count += merge_sort(sequence, l, m);
+        count += merge_sort(sequence, m + 1, r);
+        count += merge(sequence, l, m, r);
+    }
+
+    return count;
+}
+int main(int argc, char const *argv[])
+{
+    int input_size;
+    std::cin >> input_size;
+
+    vector<int> sequence(input_size, 0);
+
+    for (size_t i = 0; i < sequence.size(); ++i)
+    {
+        std::cin >> sequence.at(i);
+    }
+
+    int count = merge_sort(sequence, 0, input_size - 1);
+
+    // for (size_t i = 0; i < sequence.size(); ++i)
+    // {
+    //     std::cout << sequence.at(i) << ' ';
+    // }
+
+    cout << count;
+
+    return 0;
+}
